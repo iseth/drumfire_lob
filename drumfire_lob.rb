d829c0111a8e7a0bc3af3c7a9873a45c80641de9
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-11-25 10:39:13 +0100

    Sliming everything

--- drumfire_lob.rb
+++ drumfire_lob.rb
@@ -1,6 +1,30 @@
 require "minitest/autorun"
 
 module DrumfireLob
+  class Form
+    def <<(question_or_connector)
+    end
+    
+    def current_question
+    end
+    
+    def filling
+     yield self
+    end
+  end
+  
+  class Question
+    def initialize label
+      @label = label
+    end
+  end
+  
+  class Branching
+    def initialize(source:, yes:, no:)
+      @source, @yes, @no = source, yes, no
+    end
+  end
+
   class IntegrationTest < Minitest::Test
     def test_2_way_branching
       form = Form.new
@@ -8,7 +32,7 @@ module DrumfireLob
       question1 = Question.new "Are you 18 or more?" # Implying that all questions are boolean ATM
       question2 = Question.new "Fancy some Swiss mulled wine?"
       question3 = Question.new "Fancy some Swiss chocolate?"
-      branching = Branching.new(source: question1, yes: questions2, no: question3) # Implying that branching conditions are always yes/no
+      branching = Branching.new(source: question1, yes: question2, no: question3) # Implying that branching conditions are always yes/no
       
       form << question1
       form << branching
