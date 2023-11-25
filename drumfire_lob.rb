5a97aaa556212a69a2e89d7876844cdad466575d
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-11-25 10:28:51 +0100

    Nice-looking integration test
    The suggested API may be a bit ambitious for a first pass, though.

--- drumfire_lob.rb
+++ drumfire_lob.rb
@@ -1,8 +1,31 @@
 require "minitest/autorun"
 
-
-class SethTest < Minitest::Test
-  def test_2_way_branching
-    
+module DrumfireLob
+  class IntegrationTest < Minitest::Test
+    def test_2_way_branching
+      form = Form.new
+      # setup
+      question1 = Question.new "Are you 18 or more?" # Implying that all questions are boolean ATM
+      question2 = Question.new "Fancy some Swiss mulled wine?"
+      question3 = Question.new "Fancy some Swiss chocolate?"
+      branching = Branching.new(source: question1, yes: questions2, no: question3) # Implying that branching conditions are always yes/no
+      
+      form << question1
+      form << branching
+      
+      # assert & execute
+      form.filling do |f|
+        assert_equal "Are you 18 or more?", f.current_question
+        
+        f.answer :yes
+        assert_equal "Fancy some Swiss mulled wine?", f.current_question
+        
+        f.rollback
+        assert_equal "Are you 18 or more?", f.current_question
+        
+        f.answer :no
+        assert_equal "Fancy some Swiss chocolate?", f.current_question
+      end
+    end
   end
 end
